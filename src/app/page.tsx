cd062a03381045d056c53b2ad510cde6ba068470
Maksym Turlo <maksklas_1@ukr.net>
2023-12-27 22:46:32 +0200

    Starting page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,113 +1,81 @@
-import Image from 'next/image'
+import MaxWidthWrapper from "@/components/MaxWidth";
+import { Button, buttonVariants } from "@/components/ui/button";
+import Image from "next/image";
+import Link from "next/link";
+import "../../src/globals.css";
+import { BiArrowToBottom } from 'react-icons/bi';
+import { RiShieldCheckLine } from 'react-icons/ri';
+import { FaLayerGroup } from 'react-icons/fa';
+import { AiOutlineAntDesign } from 'react-icons/ai';
 
+const perks = [
+  {
+    name: "Instant delivery",
+    Icon: BiArrowToBottom,
+    description:
+      "Receive your email-delivered asset and download it instantly.",
+  },
+  {
+    name: "Ensured quality",
+    Icon: RiShieldCheckLine,
+    description:
+      "We check every asset on our platform to make sure it meets our high-quality standards.",
+  },
+  {
+    name: "Wide Variety",
+    Icon: FaLayerGroup,
+    description:
+      "Explore a vast selection of digital assets, ranging from graphics and templates to audio and video files. Our marketplace offers a diverse collection to suit your creative needs.",
+  },
+  {
+    name: "User-Friendly Interface",
+    Icon: AiOutlineAntDesign,
+    description:
+      "Enjoy a seamless browsing and shopping experience with our intuitive user interface. Easily navigate through categories, find what you need, and make purchases effortlessly.",
+  },
+];
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+    <>
+      <MaxWidthWrapper>
+        <div className="py-20 mx-auto text-center  flex flex-col items-center max-w-3xl">
+          <h1 className="text-4xl font-bold tracking-tight sm:text-5xl  my-20 items-center">
+            Your digital marketplace with {""}
+            <span className="text-rose-600">everything</span>.
+          </h1>
+          <p className=""></p>
+          <div className="flex flex-col sm:flex-row gap-4 my-6">
+            <Link href="/products" className={buttonVariants()}>
+              Browse for more
+            </Link>
+          </div>
+          <Button variant="ghost">Assurance of Quality &rarr;</Button>
         </div>
-      </div>
-
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore starter templates for Next.js.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
-  )
+        {/* TODO: List Products */}
+      </MaxWidthWrapper>
+      <section className="border-t border-gray-200 bg-gray-100">
+        <MaxWidthWrapper>
+          <div className="grid grid-cols-1 gap-y-12 sm:gap-x-6 sm:grid-cols-2 lg:grid-cols-2 lg:gap-x-8 lg:gap-y-8 ">
+            {perks.map((perk)=>(
+              <div key={perk.name} className="text-center md:items-start md:text-left lg:block lg:text-left bg-gradient-to-br from-rose-300 to-red-400 p-12 rounded-lg ">
+                <div className="mg:flex-shrink-0 flex justify-center">
+                  <div className="h-16 w-16 flex items-center justify-center rounded-full bg-rose-100 text-rose-900">
+                    {<perk.Icon className='w-1/3 h-1/3' />}
+                  </div>
+                </div>
+                <div className="mt-6 md:ml-4 md:mt-8 lg:ml-0 lg:mt-6">
+                  <h3 className="text-base font-medium text-slate-950">
+                    {perk.name}
+                  </h3>
+                </div>
+                <p className="mt-4 text-sm text-muted-foreground text-slate-800">
+                  {perk.description}
+                </p>
+              </div>
+            ))}
+          </div>
+        </MaxWidthWrapper>
+      </section>
+    </>
+  );
 }
